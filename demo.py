e2752cab9da2189ef051a4b460ee987219dd7329
cyruszzhou <zhouzk@uchicago.edu>
2023-07-25 21:12:16 -0500

    update demo code

--- demo.py
+++ demo.py
@@ -1,39 +1,61 @@
-import random
+"""
+Demo version for the Proposal for Rule Initialization and Adjustment for Sensitive File Detection and Group Recommendation in Cloud Storage
 
-# Initialization phase: rules are simple lambda functions for illustration.
-# In reality, you would use the results from the SAT solver here.
-rules = [
-    {
-        "rule": lambda x: x > 0.5,
-        "weight": 1.0
-    },
-    {
-        "rule": lambda x: x < 0.5,
-        "weight": 1.0
-    },
-    # More rules...
-]
+NOTE: This script is a mock-up and contains placeholder functions to illustrate the overall structure of the proposed method. In the actual implementation, these placeholder functions need to be replaced with real, working versions. Furthermore, the actual implementation will need to ensure appropriate data handling, anonymization, and user privacy protection.
+"""
 
-# Define the rewards and penalties
-reward_tp = 1.0
-reward_tn = 1.0
-penalty_fp = -1.0
-penalty_fn = -1.0
+import numpy as np
+from sklearn.model_selection import train_test_split
 
-# The history of user actions (e.g., deletions). This is just dummy data.
-user_actions = [(0.1, 0), (0.7, 1), (0.4, 0), (0.6, 1), (0.3, 0), (0.8, 1)] # (action, label)
+# Assume we have a data file with user study results.
+data_file = 'user_study_data.csv'
 
-# Reinforcement learning phase
-for action, label in user_actions:
-    for rule in rules:
-        prediction = rule["rule"](action)
-        if prediction == label:
-            # True positive or true negative: Increase the rule's weight
-            rule["weight"] += reward_tp if label == 1 else reward_tn
-        else:
-            # False positive or false negative: Decrease the rule's weight
-            rule["weight"] += penalty_fp if prediction == 1 else penalty_fn
+# Loading user study data
+def load_user_study_data(data_file):
+    # This function would typically load and preprocess the data.
+    # For this mock-up, we'll assume it returns a tuple with feature data (X) and labels (y).
+    X = np.random.rand(100, 5) # random data for 100 files and 5 features
+    y = np.random.randint(2, size=100) # random binary labels
+    return X, y
 
-# Now the rules have been adjusted based on the feedback
-for rule in rules:
-    print(f"Rule: {rule['rule'].__name__}, Weight: {rule['weight']}")
+X, y = load_user_study_data(data_file)
+
+# Splitting the data into training and validation sets.
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Rule extraction using Inductive Logic Programming (ILP)
+def extract_rules(X, y):
+    # This function would use ILP techniques to derive rules.
+    # Here we are assuming a non-existent function `ilp_extract_rules`
+    rules = ilp_extract_rules(X, y)
+    return rules
+
+rules = extract_rules(X_train, y_train)
+
+# Initial weights assignment in validation phase
+def assign_initial_weights(rules, X, y):
+    weights = np.zeros(len(rules))
+    for i, rule in enumerate(rules):
+        for x, label in zip(X, y):
+            prediction = classify_with_rule(rule, x) # Assume a function `classify_with_rule`
+            if prediction == label:
+                weights[i] += 1 # Initial weight based on rule validation
+    return weights
+
+weights = assign_initial_weights(rules, X_val, y_val)
+
+# Reinforcement learning phase (for the first phase, individual sensitive file detection)
+def update_weights(rules, weights, X, y):
+    for i, rule in enumerate(rules):
+        for x, label in zip(X, y):
+            prediction = classify_with_rule(rule, x) # Assume a function `classify_with_rule`
+            if prediction == label:
+                weights[i] += 1 # Reward: Increase weight
+            else:
+                weights[i] -= 1 # Penalty: Decrease weight
+
+# Imagine that we have an ongoing process collecting user feedback in real time.
+# Let's say the new feedback is stored in 'new_feedback.csv'
+while True:
+    new_X, new_y = load_user_study_data('new_feedback.csv')
+    update_weights(rules, weights, new_X, new_y)
